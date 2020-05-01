94c856991ca995a495f603cb9de594fcbbb2258a
Rebecca Elyanow <rebecca_elyanow@brown.edu>
2020-04-30 20:33:03 -0700

    Update run_STITCH.py

--- run_STITCH.py
+++ run_STITCH.py
@@ -5,34 +5,34 @@ import argparse
 
 parser = argparse.ArgumentParser()
  
-parser.add_argument('-method','--method',required=True,type=str,help="method (hmm or hmrf)") 
-parser.add_argument('-t','--t',required=True,type=float,help="gene") 
-parser.add_argument('-beta_spot','--beta_spot',required=True,type=float,help="spot")  
-parser.add_argument('-n_clusters','--n_clusters',required=True,type=int,help="spot")   
-parser.add_argument('-n','--name',required=True,type=str,help="name of input file (either expression matrix in .csv, .tsv, .txt format or 10X directory containing barcodes.tsv, features.tsv, matrix.mtx, and tissue_positions_list.csv)")   
+parser.add_argument('-t','--threads',required=False,type=float,default=4,help="threads") 
+parser.add_argument('-beta_spot','--beta_spot',required=False,default=2.0,type=float,help="spot")  
+parser.add_argument('-c','--n_clusters',required=True,type=int,help="number of clones")   
+parser.add_argument('-i','--input',required=True,type=str,help="name of input file (either expression matrix in .csv, .tsv, .txt format or 10X directory containing barcodes.tsv, features.tsv, matrix.mtx, and tissue_positions_list.csv)")   
 parser.add_argument('-normal_spots','--normal_spots',required=False,type=str,help="name of input file containing indices of normal spots",default=0)
 parser.add_argument('-returnnormal','--returnnormal',required=False,type=int,default=1)   
-parser.add_argument('-o','--output',required=False,type=str,default='output') 
+parser.add_argument('-o','--output',required=False,type=str,default='STITCH_output',help='output name (ex. prostate1)') 
+parser.add_argument('-outdir','--outdir',required=False,type=str,default='.',help='output directory') 
+parser.add_argument('-m','--gene_mapping_file_name',required=False,type=str,default='hgTables_hg19.txt',help='gene mapping file name') 
 args = parser.parse_args()
 
-method = args.method
-t = args.t
+nthreads = args.threads
 beta_spot = args.beta_spot
 n_clusters = args.n_clusters
 returnnormal = args.returnnormal
-name = args.name
+i = args.input
 normal_spots = args.normal_spots
 out = args.output
+gene_mapping_file_name = args.gene_mapping_file_name
+outdir = args.outdir
 
 if normal_spots !=0:
 	normal_spots = np.asarray(pd.read_csv(normal_spots,header=None)).flatten()
 else:
 	normal_spots = []
 
-operator = STITCH(name,n_clusters=n_clusters,num_states=3,normal_spots=normal_spots,beta_spots = beta_spot,nthreads=20,gene_mapping_file_name='hgTables.txt')
-posteriors = operator.callCNA(t=t,beta_spots=beta_spot,nthreads=20,maxiters=20,returnnormal=returnnormal)
+operator = STITCH(i,n_clusters=n_clusters,num_states=3,normal_spots=normal_spots,beta_spots = beta_spot,nthreads=nthreads,gene_mapping_file_name=gene_mapping_file_name)
+posteriors = operator.callCNA(beta_spots=beta_spot,nthreads=nthreads,returnnormal=returnnormal)
 
-print('Posterior Energy: ',posteriors)
-
-pd.DataFrame(operator.states).to_csv('/n/fs/ragr-research/projects/spatial_transcriptomics/states_%s_%s.csv'%(out,method),sep=',')
-pd.DataFrame(operator.labels).to_csv('/n/fs/ragr-research/projects/spatial_transcriptomics/labels_%s_%s.csv'%(out,method),sep=',')
+pd.DataFrame(operator.states).to_csv('%s/states_%s.csv'%(outdir,out),sep=',')
+pd.DataFrame(operator.labels).to_csv('%s/labels_%s.csv'%(outdir,out),sep=',')
