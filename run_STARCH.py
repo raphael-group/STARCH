2e9cba63219b8cf772ef21a29cf90a283fa06e92
Rebecca Elyanow <rebecca_elyanow@brown.edu>
2020-06-17 11:03:47 -0700

    Update and rename run_STITCH.py to run_STARCH.py

--- run_STARCH.py
+++ run_STARCH.py
@@ -1,14 +1,14 @@
-from STITCH import STITCH
+from STARCH import STARCH
 import numpy as np
 import pandas as pd
 import argparse
 
 parser = argparse.ArgumentParser()
  
-parser.add_argument('-t','--threads',required=False,type=float,default=4,help="threads") 
+parser.add_argument('-t','--threads',required=False,type=float,default=0,help="threads") 
 parser.add_argument('-beta_spot','--beta_spot',required=False,default=2.0,type=float,help="spot")  
 parser.add_argument('-c','--n_clusters',required=True,type=int,help="number of clones")   
-parser.add_argument('-i','--input',required=True,type=str,help="name of input file (either expression matrix in .csv, .tsv, .txt format or 10X directory containing barcodes.tsv, features.tsv, matrix.mtx, and tissue_positions_list.csv)")   
+parser.add_argument('-i','--input',required=True,nargs='+',help="name of input file (either expression matrix in .csv, .tsv, .txt format or 10X directory containing barcodes.tsv, features.tsv, matrix.mtx, and tissue_positions_list.csv)")   
 parser.add_argument('-normal_spots','--normal_spots',required=False,type=str,help="name of input file containing indices of normal spots",default=0)
 parser.add_argument('-returnnormal','--returnnormal',required=False,type=int,default=1)   
 parser.add_argument('-o','--output',required=False,type=str,default='STITCH_output',help='output name (ex. prostate1)') 
@@ -31,7 +31,7 @@ if normal_spots !=0:
 else:
 	normal_spots = []
 
-operator = STITCH(i,n_clusters=n_clusters,num_states=3,normal_spots=normal_spots,beta_spots = beta_spot,nthreads=nthreads,gene_mapping_file_name=gene_mapping_file_name)
+operator = STARCH(i,n_clusters=n_clusters,num_states=3,normal_spots=normal_spots,beta_spots = beta_spot,nthreads=nthreads,gene_mapping_file_name=gene_mapping_file_name)
 posteriors = operator.callCNA(beta_spots=beta_spot,nthreads=nthreads,returnnormal=returnnormal)
 
 pd.DataFrame(operator.states).to_csv('%s/states_%s.csv'%(outdir,out),sep=',')
